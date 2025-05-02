9fb20af226ff0002a57ad5eacc03d7f7aef5730b
m <m>
2025-05-02 11:05:47 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,190 @@
+// // 1-masala //
+
+// // const password = "spaceadventure2023";
+
+// // const str = password.startsWith("space") && password.endsWith("2023");
+
+// // console.log(str); 
+
+// // 2-masala //
+
+// // const message = "secret code revealed";
+
+// // if (message.includes("code")) {
+// //   const newMessage = message.toUpperCase();
+// //   console.log(newMessage);
+// // }
+
+// // 3-masala //
+
+// // const hero ="spider";
+// // const hero2 = hero.toUpperCase().concat("MAN");
+
+// // console.log(hero2);
+
+// // 4-masala //
+
+// // const ninjaCode = "   STEALTH MODE";
+// // const ninjaCode2 = ninjaCode.toLowerCase().trimStart();
+
+
+// // console.log(ninjaCode2); 
+
+// // 6-masala //
+
+// function amplifySong(song) {
+//     const str = (song + ' ').repeat(3).trim();
+//    return str.toUpperCase();
+//   }
+//    const song = "la la la";
+//   console.log(amplifySong(song));
+  
+// //   7-masala //
+
+// // const team = ["Messi", "Ronaldo"];
+// // team.push("Neymar", "Mbappe"); 
+// // team.reverse(); 
+
+// // console.log(team); 
+
+// // 8-masala //
+
+// // function shrinkTeam(team) {
+// //     team.pop(); 
+// //     return team.join(","); 
+// //   }
+  
+ 
+// //   const team = ["Messi", "Ronaldo", "Neymar"];
+// //   console.log(shrinkTeam(team));
+  
+// // 9-masala //
+
+// function mixColors(colors) {
+//     return colors.reverse().join(" "); 
+//   }
+  
+//   const colors = ["red", "blue", "green"];
+//   console.log(mixColors(colors));
+
+// 10-masala //
+  
+// function checkTeam(mos) {
+//     if (mos.includes("Messi")) {
+//       return mos.join(",");
+//     }
+//   }
+  
+
+//   const mos = ["Messi", "Ronaldo", "Neymar"];
+//   console.log(checkTeam(mos)); 
+  
+// 11-masala //
+
+// function combineAndUpper(str1, str2) {
+//     return str1.concat(str2).toUpperCase();
+//   }
+  
+ 
+//   const str1 = "Zafar";
+//   const str2 = "jon";
+//   console.log(combineAndUpper(str1, str2));
+  
+// 12-masala //
+
+// function reversePassword(password) {
+//     if (password.includes("secret")) {
+//       return password.toUpperCase();
+//     }
+//   }
+  
+//   const password = "mysecretpassword"; 
+//   console.log(reversePassword(password));
+  
+// 13-masala //
+
+// function starTeam(ism) {
+//     ism.unshift("Mbappe");
+//     return ism.reverse(); 
+//   }
+  
+//   const team = ["Messi", "Ronaldo"];
+//   console.log(starTeam(team)); 
+
+// 14-masala //
+
+// function removeFirstAndJoin(nom) {
+//     nom.shift(); 
+//     return nom.join(" ");
+//   }
+  
+  
+//   const nom = ["Messi", "Ronaldo", "Neymar"];
+//   console.log(removeFirstAndJoin(nom)); 
+  
+// 15-masala //
+
+// function fixCode(kod) {
+//     if (kod.search("error") !== -1) {
+//       return kod.toUpperCase();
+//     }
+//     return "Xatolik topilmadi 😌";
+//   }
+  
+//   const kod = "debug this error now";
+//   console.log(fixCode(kod));
+  
+// // 16-masala //
+
+// function finishCode(code) {
+//     return code.trimStart().concat("done");
+//   }
+  
+ 
+//   const code = "   write code";
+//   console.log(finishCode(code)); 
+
+// 17-masala //
+
+// function startCode(code) {
+//     return "start".concat(code.trimEnd());
+//   }
+  
+//   const code = "code now   ";
+//   console.log(startCode(code)); 
+  
+// 18-masala //
+
+
+// function checkAndUpperName(name) {
+//     if (name.endsWith("bek")) {
+//       return name.toUpperCase();
+//     }
+//     return name; 
+//   }
+  
+//   const name = "Fayzullobek";
+//   console.log(checkAndUpperName(name)); 
+  
+// 19-masala //
+
+
+// function checkAndJoinFoods(foods) {
+//     if (foods.includes("pizza")) {
+//       return foods.reverse().join(" ");
+//     }
+  
+//   }
+  
+ 
+//   const foods = ["burger", "pizza", "sushi"];
+//   console.log(checkAndJoinFoods(foods)); 
+  
+// 20-masala //
+// function checkCodeInName(names) {
+//     return names.toUpperCase().includes("CODE");
+//   }
+  
+//   const names = "zafarcodejon";
+//   console.log(checkCodeInName(names)); 
+  
